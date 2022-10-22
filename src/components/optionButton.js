07e2554720c84afa43b822c31fe2e3634d200711
Grace-Shao <graceshao203@gmail.com>
2022-10-22 11:23:17 -0400

    optionButton half finished

--- src/components/optionButton.js
+++ src/components/optionButton.js
@@ -7,6 +7,10 @@ class OptionButton extends Component {
             <button onClick={() => {alert(this.props.thisButtonClicks)}}>{ this.props.option1}</button>
         )
     }
+    // if option 1, have the bg color be red
+    
+    // if option 2, have the bg color be blue
+    // if option 3, have the bg color be purple
 }
 
 export default OptionButton;
