643016735f2b96cdef99a659a3ac64cb572a5a80
Cynthia7979 <CynthiaWang7979@hotmail.com>
2022-10-22 02:19:05 -0400

    feat: Upload field styling done

--- src/components/uploadField.js
+++ src/components/uploadField.js
@@ -1,10 +1,44 @@
 import './uploadField.css';
-import { Component } from 'react';
+import { Component, createRef } from 'react';
+
+const allowedFileExts = ['png', 'jpg', 'jpeg']
 
 class UploadField extends Component {
+    state = {
+        selectedFile: null,
+        fileUploadRef: createRef(),
+        uploadSVGRef: createRef()
+    }
+
+    handleClick() {
+        this.state.fileUploadRef.current.click()
+    }
+
+    onFileChange(event) {
+        const fileObj = event.target.files[0]
+        const fileExt = fileObj.name.split('.').pop()
+        if (!allowedFileExts.includes(fileExt.toLowerCase())) {
+            alert('Invalid file extension! Please choose a different file.')
+        } else {
+            this.setState({selectedFile: event.target.files[0].name})
+        }
+    }
+
     render() {
         return (
-            <p onClick={() => {alert(this.props.hello)}}>{ this.props.hello }</p>
+            <div id="upload-field" onClick={this.handleClick.bind(this)}>
+                <object
+                    id="upload-icon" data="/upload.svg" aria-label="Upload File"
+                    onClick={this.handleClick.bind(this)}
+                />
+
+                <p id="upload-text">Choose File</p>
+
+                <input
+                    type="file" ref={this.state.fileUploadRef}
+                    onChange={this.onFileChange.bind(this)} style={{display:'none'}}
+                />
+            </div>
         )
     }
 }
